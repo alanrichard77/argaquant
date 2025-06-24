2812b4111794350984f0940e66e10828f452f701
alanrichard77 <alanrichard_@outlook.com>
2025-06-24 19:28:40 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
-from flask import Flask, render_template, request
-from utils.get_data import get_company_data
-
+from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query')
-    data = get_company_data(query)
-    if data:
-        return render_template('company.html', data=data)
-    else:
-        return render_template('not_found.html', query=query)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def home(): return 'ARGA QUANT ONLINE'
